40e0d16b4d9756ae533fcd38f514fcbaf1668dd7
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-05-12 23:36:21 +0200

    modify size round card

--- src/professionnal/listprofessionnal/Listprofessionnal.js
+++ src/professionnal/listprofessionnal/Listprofessionnal.js
@@ -9,7 +9,7 @@ function Listprofessionnal () {
             {portfolio.map((data) => {
                 return (
                     <div className="row d-flex justify-content-center align-items-center my-5">
-                        <div className="col-6 text-center dates mb-2 me-lg-3">
+                        <div className="col-6 text-center d-flex justify-content-center align-items-center dates mb-2 me-lg-3">
                             <h6 className="p-3">{data.de}<br/> {data.à}</h6>
                         </div>
                         <div className="col-6 text-center parcours text-dark">
